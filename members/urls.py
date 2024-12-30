5ada02045ab1a97766826df452636d4093d11912
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2024-12-30 16:33:53 +0800

    workign on master template

--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('members/', views.members, name="members"),
-    path('users/', views.users, name='users')
+    path('users/', views.users, name='users'),
+    path('members/details/<int:id>', views.member_details, name="details")
 ]
--- members/views.py
+++ members/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.template import loader
+from  .models import Member
 # Create your views here.
 
 
-def members(request):
-    template = loader.get_template("myfirst.html")
-    return HttpResponse(template.render())
+def members(request: HttpRequest):
+    members = Member.objects.all().values()
+    template = loader.get_template("all_members.html")
+    context = {
+        'mymembers': members,
+     
+    }
+    print(request)
+    return HttpResponse(template.render(context, request))
 
 def users(request):
     return HttpResponse("users page")
+
+def member_details(request: HttpRequest ,id):
+    find_member = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    
+    context = {
+        'mymember' : find_member
+    }
+    return HttpResponse(template.render(context,request))
